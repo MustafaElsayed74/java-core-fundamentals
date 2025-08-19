4b33e84edf1a0577c88dec48e3b3fb019a3c4d6b
0x0desha74 <mudesha74@gmail.com>
2025-08-19 03:07:30 +0300

    Java Fundamentals

---
+++ demo.java
@@ -0,0 +1,20 @@
+
+public class demo {
+    public static void main(String args[]) {
+
+        int nums[][] = new int[3][4];
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 3; j++) {
+                nums[i][j] = (int) (Math.random() * 100);
+            }
+        }
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 3; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
