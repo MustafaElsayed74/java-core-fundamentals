88dbc7d8655a4afa3b0c49225745d748656ac195
0x0desha74 <mudesha74@gmail.com>
2025-08-22 03:02:46 +0300

    Object Class

--- demo.java
+++ demo.java
@@ -1,10 +1,54 @@
 
+class Laptop {
+    String brand;
+    double price;
+
+    @Override
+    public String toString() {
+        return Laptop.class.getName() + " [brand=" + brand + ", price=" + price + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((brand == null) ? 0 : brand.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(price);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (brand == null) {
+            if (other.brand != null)
+                return false;
+        } else if (!brand.equals(other.brand))
+            return false;
+        if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+            return false;
+        return true;
+    }
+
+}
+
 public class demo {
 
     public static void main(String args[]) {
-        Calculator calculator = new Calculator();
-        int r1 = calculator.add(1, 2);
-        System.out.println(r1);
+
+        Laptop laptop = new Laptop();
+
+        laptop.brand = "MSI";
+        laptop.price = 40000;
+        System.out.println(laptop);
     }
 
 }
