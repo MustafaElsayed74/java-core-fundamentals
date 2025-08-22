58ef2dd0fec79b03d4572831a0bfff9f2cd84958
0x0desha74 <mudesha74@gmail.com>
2025-08-22 03:40:05 +0300

    Abstract Class

--- demo.java
+++ demo.java
@@ -1,41 +1,16 @@
+abstract class Car {
+    abstract void drive();
 
-class Laptop {
-    String brand;
-    double price;
-
-    @Override
-    public String toString() {
-        return Laptop.class.getName() + " [brand=" + brand + ", price=" + price + "]";
+    public static void playMusic() {
+        System.out.println("Playing Music..");
     }
+}
+
+class Ford extends Car {
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((brand == null) ? 0 : brand.hashCode());
-        long temp;
-        temp = Double.doubleToLongBits(price);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Laptop other = (Laptop) obj;
-        if (brand == null) {
-            if (other.brand != null)
-                return false;
-        } else if (!brand.equals(other.brand))
-            return false;
-        if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
-            return false;
-        return true;
+    void drive() {
+        System.out.println("Driving..");
     }
 
 }
@@ -44,11 +19,9 @@ public class demo {
 
     public static void main(String args[]) {
 
-        Laptop laptop = new Laptop();
-
-        laptop.brand = "MSI";
-        laptop.price = 40000;
-        System.out.println(laptop);
+        // Abstract class cannot be instantiated,we can create an object from abstract
+        // class
+        Car.playMusic();
     }
 
 }
