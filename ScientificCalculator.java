cd4fa7a5aae6457138a5ff395840664e5656e876
0x0desha74 <mudesha74@gmail.com>
2025-08-24 18:45:51 +0300

    Multiple threads using Runnable functional interface, and using Tread class

--- ScientificCalculator.java
+++
@@ -1,6 +0,0 @@
-public class ScientificCalculator extends AdvCalculator {
-
-    public double power(double a, double b) {
-        return Math.pow(a, b);
-    }
-}
--- demo.java
+++ demo.java
@@ -1,27 +1,59 @@
-abstract class Car {
-    abstract void drive();
+// class A implements Runnable {
+//     public void run() {
+//         for (int i = 0; i <= 10; i++) {
+//             System.out.println(" Hi");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
 
-    public static void playMusic() {
-        System.out.println("Playing Music..");
-    }
-}
+// }
 
-class Ford extends Car {
-
-    @Override
-    void drive() {
-        System.out.println("Driving..");
-    }
-
-}
+// class B extends A implements Runnable {
+//     public void run() {
+//         for (int i = 0; i <= 10; i++) {
+//             System.out.println(" Hello");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
 
 public class demo {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
+        Runnable obj1 = () -> {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(" Hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
-        // Abstract class cannot be instantiated,we can create an object from abstract
-        // class
-        Car.playMusic();
+        Runnable obj2 = () -> {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(" Hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
     }
-
 }
